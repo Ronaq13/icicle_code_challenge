4585abff3d9ac99bb5a6166c018d45ff7d6e92a8
Ronaq13 <raounaqsharma@gmail.com>
2019-04-23 21:06:26 +0530

    Minor Update
    Wrote some initial test for a generator. And made
    a little architecture of the application.
    Also added a keypad helper which will map key numbers to
    their respective alphabets.

---
+++ lib/generate_words.rb
@@ -0,0 +1,5 @@
+require_relative './../helpers/keypad'
+
+class GenerateWords
+  
+end
---
+++ spec/lib/generate_words_spec.rb
@@ -0,0 +1,10 @@
+require_relative './../../lib/generate_words'
+
+RSpec.describe 'GenerateWords' do
+  it 'generates words for a given number'
+  it 'generate words having minimum length of 3 words'
+
+  context 'when wrong input is given' do
+    it 'gives a clean error message'
+  end
+end
